22bed66846297467d1621d3d078dab87433ca768
James Polk <james@docupletionforms.com>
2023-01-11 05:11:55 -0800

    2. Smoke Test

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -18,5 +24,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.9",
+  networks: {
+    localhost: {}
+  },
 };
