48e47f6a5168ca9cff0bb778d704699978446308
James Polk <james@docupletionforms.com>
2023-01-19 17:38:57 -0800

    7. Token transfer delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -28,29 +28,45 @@ contract Token {
         string memory _name,
         string memory _symbol,
         uint256 _totalSupply
-    ) {
+    ) 
+    {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint256 _value)
+    function transfer(
+        address _to, 
+        uint256 _value
+    )
         public 
         returns (bool success)
     {
         require(balanceOf[msg.sender] >= _value); 
-        require(_to != address(0));
 
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-        balanceOf[_to] = balanceOf[_to] + _value;
-
-        emit Transfer(msg.sender, _to, _value);
+        _transfer(msg.sender, _to, _value);
 
         return true;
     }
 
-    function approve(address _spender, uint256 _value)
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _value
+    ) internal {
+        require(_to != address(0));
+
+        balanceOf[_from] = balanceOf[_from] - _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        emit Transfer(_from, _to, _value);
+    }
+
+    function approve(
+        address _spender, 
+        uint256 _value
+    )
         public 
         returns (bool success)
     {
@@ -61,4 +77,23 @@ contract Token {
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
+
+    function transferFrom(
+        address _from, 
+        address _to, 
+        uint256 _value
+    )
+        public
+        returns (bool success)
+    {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+
+        _transfer(_from, _to, _value);
+
+        return true;
+    }
+
 }
